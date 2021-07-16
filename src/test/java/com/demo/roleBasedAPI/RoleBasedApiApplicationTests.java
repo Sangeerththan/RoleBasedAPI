b1215ae568c817249e16fdae66e72bc48b08e72b
sangeerththan <sangeerththan.15@cse.mrt.ac.lk>
2021-07-16 13:33:37 +0530

    Spring Boot Dependencies for API

---
+++ src/test/java/com/demo/roleBasedAPI/RoleBasedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.roleBasedAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
